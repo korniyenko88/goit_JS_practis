ceb78d59a85791a8909ccf3518369764783000fd
unknown <you@example.com>
2024-07-22 16:50:47 +0300

    update module 4

--- js/index.js
+++ js/index.js
@@ -245,3 +245,88 @@
 // console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 // console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 // console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// 4. Mодуль 4. Об'єкти
+
+// Task 1
+
+// Задача 1. Пакування товарів
+// Напиши функцію isEnoughCapacity(products, containerSize), 
+// яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
+// Функція оголошує два параметри:
+// products — об’єкт, у якому ключі містять назви товарів, 
+// а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, 
+// яку в себе може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. 
+// Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
+// function isEnoughCapacity(products, containerSize) {
+//     let totalQuantity = 0;
+//     for (const product in products) {
+//       totalQuantity += products[product];
+//     }
+//     return totalQuantity <= containerSize;
+//   }
+
+
+
+// console.log(
+//     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+//   ); // true
+  
+//   console.log(
+//     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+//   ); // false
+  
+//   console.log(
+//     isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+//   ); // true
+  
+//   console.log(
+//     isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+//   ); // false
+
+// Task 2
+
+// Задача 2. Розрахунок калорій
+// Напиши функцію calcAverageCalories(days), 
+// яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня. 
+// Функція очікує один параметр: days — масив об’єктів. 
+// Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день. 
+
+function calcAverageCalories(days){
+    if (days.length ===0) return 0; 
+let totalCalories = 0;
+for (const day of days){
+    totalCalories += day.calories;
+}
+return totalCalories / days.length;
+}
+console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 3010 },
+      { day: "tuesday", calories: 3200 },
+      { day: "wednesday", calories: 3120 },
+      { day: "thursday", calories: 2900 },
+      { day: "friday", calories: 3450 },
+      { day: "saturday", calories: 3280 },
+      { day: "sunday", calories: 3300 }
+    ])
+  ); // 3180
+  
+  console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 2040 },
+      { day: "tuesday", calories: 2270 },
+      { day: "wednesday", calories: 2420 },
+      { day: "thursday", calories: 1900 },
+      { day: "friday", calories: 2370 },
+      { day: "saturday", calories: 2280 },
+      { day: "sunday", calories: 2610 }
+    ])
+  ); // 2270
+  
+  console.log(
+    calcAverageCalories([])
+  ); // 0
