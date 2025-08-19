7500367c2391bb5eedd7d1b9b9e99172aaffdb62
Volodymyr <korniyenko88@gmail.com>
2025-08-19 15:49:00 +0300

    practis

--- js/index.js
+++ js/index.js
@@ -251,14 +251,14 @@
 // Task 1
 
 // Задача 1. Пакування товарів
-// Напиши функцію isEnoughCapacity(products, containerSize), 
+// Напиши функцію isEnoughCapacity(products, containerSize),
 // яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
 // Функція оголошує два параметри:
-// products — об’єкт, у якому ключі містять назви товарів, 
+// products — об’єкт, у якому ключі містять назви товарів,
 // а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
-// containerSize — число, максимальна кількість одиниць товарів, 
+// containerSize — число, максимальна кількість одиниць товарів,
 // яку в себе може вмістити контейнер.
-// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. 
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер.
 // Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
 // function isEnoughCapacity(products, containerSize) {
@@ -269,20 +269,18 @@
 //     return totalQuantity <= containerSize;
 //   }
 
-
-
 // console.log(
 //     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
 //   ); // true
-  
+
 //   console.log(
 //     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
 //   ); // false
-  
+
 //   console.log(
 //     isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
 //   ); // true
-  
+
 //   console.log(
 //     isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
 //   ); // false
@@ -290,43 +288,88 @@
 // Task 2
 
 // Задача 2. Розрахунок калорій
-// Напиши функцію calcAverageCalories(days), 
-// яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня. 
-// Функція очікує один параметр: days — масив об’єктів. 
-// Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день. 
+// Напиши функцію calcAverageCalories(days),
+// яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня.
+// Функція очікує один параметр: days — масив об’єктів.
+// Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день.
 
-function calcAverageCalories(days){
-    if (days.length ===0) return 0; 
-let totalCalories = 0;
-for (const day of days){
-    totalCalories += day.calories;
-}
-return totalCalories / days.length;
-}
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 3010 },
-      { day: "tuesday", calories: 3200 },
-      { day: "wednesday", calories: 3120 },
-      { day: "thursday", calories: 2900 },
-      { day: "friday", calories: 3450 },
-      { day: "saturday", calories: 3280 },
-      { day: "sunday", calories: 3300 }
-    ])
-  ); // 3180
+// function calcAverageCalories(days){
+//     if (days.length ===0) return 0;
+// let totalCalories = 0;
+// for (const day of days){
+//     totalCalories += day.calories;
+// }
+// return totalCalories / days.length;
+// }
+// console.log(
+//     calcAverageCalories([
+//       { day: "monday", calories: 3010 },
+//       { day: "tuesday", calories: 3200 },
+//       { day: "wednesday", calories: 3120 },
+//       { day: "thursday", calories: 2900 },
+//       { day: "friday", calories: 3450 },
+//       { day: "saturday", calories: 3280 },
+//       { day: "sunday", calories: 3300 }
+//     ])
+//   ); // 3180
+
+//   console.log(
+//     calcAverageCalories([
+//       { day: "monday", calories: 2040 },
+//       { day: "tuesday", calories: 2270 },
+//       { day: "wednesday", calories: 2420 },
+//       { day: "thursday", calories: 1900 },
+//       { day: "friday", calories: 2370 },
+//       { day: "saturday", calories: 2280 },
+//       { day: "sunday", calories: 2610 }
+//     ])
+//   ); // 2270
+
+//   console.log(
+//     calcAverageCalories([])
+//   ); // 0
+
+// const result = Number('123.123');
+
+// const userInput = prompt('Put your ege');
+// const mathUserInput = Number(userInput)
+// console.log("user age:", mathUserInput);
+
+// let userInput = prompt('Enter your age');
+// const userInputToNumber = Number(userInput);
+// if (typeof(userInputToNumber)=== "number") {
+//   console.log("User age:",  userInputToNumber);
+// } else {
+//   console.log("Invalid params");
+// }
+// const value = 2;
+
+// switch (value) {
+//   case 1:
+//     console.log("case 1");
+//     break
+//   case 2:
+//     console.log('case 2');
+//     break
+//   case 3:
+//     console.log("case 3");
+// }
+
+
+const deadLine = +prompt('enter day');
+if (deadLine === 0) {
+  alert('today');
   
-  console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 2040 },
-      { day: "tuesday", calories: 2270 },
-      { day: "wednesday", calories: 2420 },
-      { day: "thursday", calories: 1900 },
-      { day: "friday", calories: 2370 },
-      { day: "saturday", calories: 2280 },
-      { day: "sunday", calories: 2610 }
-    ])
-  ); // 2270
-  
-  console.log(
-    calcAverageCalories([])
-  ); // 0
+}
+else if (deadLine === 1) {
+  alert('tomorrow');
+
+}
+else if (deadLine === 2) {
+  alert('owertomorrow')
+
+}
+
+else {
+  alert('not corect days')
+}
