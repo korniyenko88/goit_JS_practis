2635c8291f9658f1516f47d0a22974d5e37ba00c
Volodymyr <korniyenko88@gmail.com>
2025-09-12 15:59:10 +0300

    practise

---
+++ js/index-2.js
@@ -0,0 +1,24 @@
+// function formatMessage(message, maxLength) {
+//   if (message.length <= maxLength) {
+//     return message;
+//   } else {
+//     return message.slice(0, maxLength) + "...";
+//   }
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 50));
+
+function getShippingCost(country) {
+  switch (country) {
+    case "China": {
+      return `Shipping to ${country} will cost 100 credits`;
+    }
+
+    case "Chile": {
+     return `Shipping to ${country} will cost 250 credits`;
+      }
+      case 'Australia': {
+        return  `Shipping to ${country} will cost 170 credits`;
+          }
+  }
+}
+console.log(getShippingCost("Australia"));
