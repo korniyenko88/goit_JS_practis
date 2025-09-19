e8744e11494be71ffda162f6509c583a48dd46f3
Vladymyr <korniyenko88@gmail.com>
2025-09-19 21:40:20 +0300

    practice

--- js/index-2.js
+++ js/index-2.js
@@ -7,18 +7,23 @@
 // }
 // console.log(formatMessage("Curabitur ligula sapien", 50));
 
-function getShippingCost(country) {
-  switch (country) {
-    case "China": {
-      return `Shipping to ${country} will cost 100 credits`;
-    }
+// function getShippingCost(country) {
+//   switch (country) {
+//     case "China": {
+//       return `Shipping to ${country} will cost 100 credits`;
+//     }
 
-    case "Chile": {
-     return `Shipping to ${country} will cost 250 credits`;
-      }
-      case 'Australia': {
-        return  `Shipping to ${country} will cost 170 credits`;
-          }
-  }
+//     case "Chile": {
+//      return `Shipping to ${country} will cost 250 credits`;
+//       }
+//       case 'Australia': {
+//         return  `Shipping to ${country} will cost 170 credits`;
+//           }
+//   }
+// }
+// console.log(getShippingCost("Australia"));
+
+const item = ['1', '2', '3', '4'] ;
+for (let i = 0; i < item.length; i++){
+    console.log(item[i]);
 }
-console.log(getShippingCost("Australia"));
