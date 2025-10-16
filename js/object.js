0dc6d5a4b82e9074ddf83c73200a7cb695754401
Volodymyr <korniyenko88@gmail.com>
2025-10-16 15:16:35 +0300

    update

--- js/object.js
+++ js/object.js
@@ -1,177 +1,177 @@
-// // const numbers = {
-// //     first: '1',
-// //     second: '2',
-// //     therd: '3'
+// // // const numbers = {
+// // //     first: '1',
+// // //     second: '2',
+// // //     therd: '3'
+// // // };
+
+// // // console.log('object: ', numbers);
+
+// // // let total = 0;
+
+// // // for (const key in numbers) {
+// // //     // console.log(`${key} : ${numbers[key]}`)
+
+// // //     total += +numbers[key];
+
+// // // }
+// // // console.log(total);
+
+// // // const keys = Object.keys(numbers)
+// // // console.log(keys)
+
+// // // let totalValue = 0;
+// // // for (const key of keys) {
+// // //     console.log(key)
+// // //     totalValue += +numbers[key];
+// // //     console.log(totalValue)
+// // // }
+// // // console.log(totalValue)
+
+// // // const user = {
+// // //   name: "Mango",
+// // //   age: 20,
+// // //   hobby: "html",
+// // //   premium: true,
+// // // };
+
+// // // console.log("User: ", user);
+
+// // // user.mood = "hapy";
+// // // user.hobby = "skydiving";
+// // // user.premium = false;
+
+// // // console.log(user);
+
+// // // const userKeys = Object.keys(user);
+// // // console.log(userKeys);
+
+// // // for (const key of userKeys) {
+// // //   console.log(`${key}: ${user[key]}`);
+// // // }
+
+// // // const obj = {
+// // //   x: 2,
+// // //   y: 3,
+// // //   z: 4,
+// // // };
+
+// // // const keys = Object.keys(obj);
+// // // console.log(keys);
+// // // for (const key of keys) {
+// // //   obj[key] = obj[key] ** 2;
+// // // }
+
+// // // console.log(obj);
+
+// // const salaries = {
+// //   John: 100,
+// //   Ann: 160,
+// //   Pete: 130,
 // // };
 
-// // console.log('object: ', numbers);
+// // let sum = 0;
 
-// // let total = 0;
+// // const values = Object.values(salaries);
+// // console.log(values);
 
-// // for (const key in numbers) {
-// //     // console.log(`${key} : ${numbers[key]}`)
-
-// //     total += +numbers[key];
+// // for (const value of values) {
+// //     sum += +value;
 
 // // }
-// // console.log(total);
+// // console.log('Total Salaries:', sum);
 
-// // const keys = Object.keys(numbers)
-// // console.log(keys)
+// // const friends = [
+// //   { name: "Mango", online: false },
+// //   { name: "Kivi", online: true },
+// //   { name: "Poly", online: true },
+// //   { name: "Ajax", online: false },
+// // ];
 
-// // let totalValue = 0;
-// // for (const key of keys) {
-// //     console.log(key)
-// //     totalValue += +numbers[key];
-// //     console.log(totalValue)
-// // }
-// // console.log(totalValue)
+// // console.log("Obj:", friends);
 
-// // const user = {
-// //   name: "Mango",
-// //   age: 20,
-// //   hobby: "html",
-// //   premium: true,
-// // };
-
-// // console.log("User: ", user);
-
-// // user.mood = "hapy";
-// // user.hobby = "skydiving";
-// // user.premium = false;
-
-// // console.log(user);
-
-// // const userKeys = Object.keys(user);
-// // console.log(userKeys);
-
-// // for (const key of userKeys) {
-// //   console.log(`${key}: ${user[key]}`);
+// // function findFriendByName(allFriends, friendName) {
+// //   for (const friend of allFriends) {
+// //     if (friend.name.toLowerCase() === friendName.toLowerCase()) {
+// //       return friend;
+// //     }
+// //   }
+// //   return ` ${friendName.toUpperCase()} not found!`;
 // // }
 
-// // const obj = {
-// //   x: 2,
-// //   y: 3,
-// //   z: 4,
-// // };
+// // console.log(findFriendByName(friends, "kivi"));
 
-// // const keys = Object.keys(obj);
-// // console.log(keys);
-// // for (const key of keys) {
-// //   obj[key] = obj[key] ** 2;
+// // function getAllNames (allFriends) {
+// //     let frindsNames = [];
+// //     for (const names of allFriends) {
+// //         frindsNames.push(names.name);
+
+// //     }
+// //     return frindsNames;
+
 // // }
 
-// // console.log(obj);
+// // console.log(getAllNames(friends));
 
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
+// // function getOnlineFriends(allFriends) {
+// //     let friendOnline = [];
+// //     for (const friend of allFriends) {
+// //         if (friend.online === true) {
+// //             friendOnline.push(friend.name);
+// //         }
+// //     }
+// //     return friendOnline;
+// // }
 
-// let sum = 0;
+// // console.log(getOnlineFriends(friends))
 
-// const values = Object.values(salaries);
-// console.log(values);
-
-// for (const value of values) {
-//     sum += +value;
-
-// }
-// console.log('Total Salaries:', sum);
-
-// const friends = [
-//   { name: "Mango", online: false },
-//   { name: "Kivi", online: true },
-//   { name: "Poly", online: true },
-//   { name: "Ajax", online: false },
+// const stones = [
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 3 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Аконіт", price: 200, quantity: 2 },
 // ];
 
-// console.log("Obj:", friends);
-
-// function findFriendByName(allFriends, friendName) {
-//   for (const friend of allFriends) {
-//     if (friend.name.toLowerCase() === friendName.toLowerCase()) {
-//       return friend;
+// function calcTotalPrice(allStones, stonesName) {
+//   let totalPrice = 0;
+//   for (const stone of allStones) {
+//     if (stone.name.toLowerCase() === stonesName.toLowerCase()) {
+//       totalPrice = stone.price * stone.quantity;
 //     }
 //   }
-//   return ` ${friendName.toUpperCase()} not found!`;
+//   return `${totalPrice} ${stonesName}`;
 // }
 
-// console.log(findFriendByName(friends, "kivi"));
+// console.log(calcTotalPrice(stones, "Діамант"));
 
-// function getAllNames (allFriends) {
-//     let frindsNames = [];
-//     for (const names of allFriends) {
-//         frindsNames.push(names.name);
+// const playList = {
+//   name: "qwerty",
+//   rating: 6,
+//   traks: ["trak-1", "trak-2", "trak-3"],
+//   changeName(newName) {
+//     this.name = newName;
+//   },
+//   updateRating(newRating) {
+//     this.rating = newRating;
+//   },
+// };
 
-//     }
-//     return frindsNames;
+// console.log(playList);
 
-// }
+// playList.changeName("some trak");
+// playList.updateRating(2);
+// console.log(playList);
 
-// console.log(getAllNames(friends));
+// const temps = [18, 14, 12, 21, 17, 29, 24];
 
-// function getOnlineFriends(allFriends) {
-//     let friendOnline = [];
-//     for (const friend of allFriends) {
-//         if (friend.online === true) {
-//             friendOnline.push(friend.name);
-//         }
-//     }
-//     return friendOnline;
-// }
+// const tempsCopy = [...temps]
 
-// console.log(getOnlineFriends(friends))
+// console.log(temps)
+// console.log('copy:', tempsCopy)
 
-const stones = [
-  { name: "Смарагд", price: 1300, quantity: 4 },
-  { name: "Діамант", price: 2700, quantity: 3 },
-  { name: "Сапфір", price: 400, quantity: 7 },
-  { name: "Аконіт", price: 200, quantity: 2 },
-];
+// temps.push(20)
+// console.log(temps);
+// console.log("copy:", tempsCopy);
 
-function calcTotalPrice(allStones, stonesName) {
-  let totalPrice = 0;
-  for (const stone of allStones) {
-    if (stone.name.toLowerCase() === stonesName.toLowerCase()) {
-      totalPrice = stone.price * stone.quantity;
-    }
-  }
-  return `${totalPrice} ${stonesName}`;
-}
+// const minTemp = Math.min(...temps) 
 
-console.log(calcTotalPrice(stones, "Діамант"));
-
-const playList = {
-  name: "qwerty",
-  rating: 6,
-  traks: ["trak-1", "trak-2", "trak-3"],
-  changeName(newName) {
-    this.name = newName;
-  },
-  updateRating(newRating) {
-    this.rating = newRating;
-  },
-};
-
-console.log(playList);
-
-playList.changeName("some trak");
-playList.updateRating(2);
-console.log(playList);
-
-const temps = [18, 14, 12, 21, 17, 29, 24];
-
-const tempsCopy = [...temps]
-
-console.log(temps)
-console.log('copy:', tempsCopy)
-
-temps.push(20)
-console.log(temps);
-console.log("copy:", tempsCopy);
-
-const minTemp = Math.min(...temps) 
-
-console.log(minTemp);
+// console.log(minTemp);
